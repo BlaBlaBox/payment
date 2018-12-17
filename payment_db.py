acd02f23efc019e66a0be4078200fb998a164a29
Alpkant <kantarcia@itu.edu.tr>
2018-12-17 18:59:35 +0300

    One function is missing

---
+++ payment_db.py
@@ -0,0 +1,90 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date, datetime
+from payment_config import app
+
+db = SQLAlchemy(app)
+#db.drop_all()
+
+class carttrans(db.Model):
+    trans_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,nullable=False)
+    trans_time = db.Column(db.DateTime, default=(datetime.now()).strftime("%Y-%m-%d %H:%M:%S"))
+    is_completed = db.Column(db.Boolean, default=False)
+
+
+
+class itemtrans(db.Model):
+    trans_id = db.Column(db.Integer,db.ForeignKey('carttrans.trans_id'),primary_key=True)
+    movie_id = db.Column(db.Integer,primary_key=True)
+    price = db.Column(db.Numeric(precision=5,scale=2))
+    duration = db.Column(db.Integer,default=-1)
+
+
+
+def add_db(obj):
+    try:
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+    except:
+        db.session.rollback()
+        raise
+
+def remove_db(obj):
+    try:
+        db.session.delete(obj)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        raise
+
+def commit_db():
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
+        raise
+
+# Not completed carts should be detected afterwards
+def add_cart(user_id):
+    cart_obj = carttrans.query.filter_by(user_id=user_id).filter_by(is_completed=False).first()
+    if cart_obj:
+        return None
+
+    new_cart = carttrans(user_id=user_id)
+    return add_db(new_cart)
+
+def complete_cart(user_id):
+    cart_obj = carttrans.query.filter_by(user_id=user_id).filter_by(is_completed=False).first()
+    cart_obj.is_completed = True 
+    commit_db()
+    return cart_obj
+
+def add_item(user_id,movie_id,price,duration):
+    cart_obj = carttrans.query.filter_by(user_id=user_id).filter_by(is_completed=False).first()
+    new_item = itemtrans(trans_id=cart_obj.trans_id,movie_id=movie_id,price=price,duration=duration)
+    return add_db(new_item)
+    
+def remove_item(user_id,movie_id):
+    cart_obj = carttrans.query.filter_by(user_id=user_id).filter_by(is_completed=False).first()
+    item = itemtrans.query.filter_by(trans_id=cart_obj.trans_id,movie_id=movie_id).first()
+    remove_db(item)
+    return True
+
+## BURADA HATA OLABILIR FRONTENDDE BAKILMASI LAZIM
+def update_item(user_id,movie_id,price,duration):
+    try:
+        cart_obj = carttrans.query.filter_by(user_id=user_id).filter_by(is_completed=False).first()
+        rent_obj = carttrans.query.filter_by(trans_id=cart_obj.trans_id).filter_by(movie_id=rent_id).first()
+        rent_obj.price = price
+        rent_obj.duration = duration 
+        db.session.commit()
+        return rent_obj
+    except:
+        db.session.rollback()
+        raise
+
+
+
+
+db.create_all()
---
+++ payment_ms.py
@@ -0,0 +1,103 @@
+from flask import jsonify, request, abort
+import requests
+from payment_config import app
+from datetime import datetime
+from payment_db import add_cart, complete_cart, add_item, remove_item, update_item
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
+
+@app.errorhandler(400)
+def not_found(error):
+    return jsonify({'error': 'Your request doesn\'t contain JSON'}), 400
+
+@auth.error_handler
+def unauthorized():
+    return jsonify({'error': 'Unauthorized access'}), 403
+
+
+# The create action of payment
+@app.route('/cart/create/<int:user_id>', methods=['POST'])
+#@auth.login_required
+def create_cart(user_id):
+    new_cart = add_cart(user_id)
+    if new_cart == None:
+        return jsonify({'error': 'There is already a cart which is not finished.'}), 400
+    return jsonify({'result': 'Success','cart_id':new_cart.trans_id}), 200
+
+@app.route('/cart/item/create/<int:user_id>', methods=['POST'])
+#@auth.login_required
+def create_item(user_id):
+    if not request.json:
+        abort(400)
+
+    movie_id = request.json['movie_id']
+    price = request.json['price']
+    duration = requests.json['duration']
+
+    new_item = add_item(user_id,movie_id,price,duration)
+    if new_item == None:
+        return jsonify({'error': 'There is a problem with creating new item.'}), 400
+    return jsonify({'result': 'Success','item_id':new_item.trans_id}), 200
+
+
+@app.route('/cart/item/delete/<int:user_id>', methods=['POST'])
+#@auth.login_required
+def delete_item(user_id):
+    if not request.json:
+        abort(400)
+
+    movie_id = request.json['movie_id']
+
+    new_item = remove_item(user_id,movie_id)
+    return jsonify({'result': 'Success'}), 200
+
+@app.route('/cart/item/update/<int:user_id>', methods=['POST'])
+#@auth.login_required
+def update_item(user_id):
+    if not request.json:
+        abort(400)
+
+    movie_id = request.json['movie_id']
+    price = request.json['price']
+    duration = requests.json['duration']
+
+    new_item = remove_item(user_id,movie_id)
+    return jsonify({'result': 'Success','item_id':new_item.trans_id}), 200
+
+
+@app.route('/payment/pay/<int:user_id>', methods=['POST'])
+#@auth.login_required
+def pay(user_id):
+    if not request.json:
+        abort(400)
+    
+    url = 'http://127.0.0.1:7000/creditcard/pay'
+
+    response = requests.post(url, json=request.json,headers={'Content-Type': 'application/json'})
+    print(response.status_code)
+    result = response.status_code
+    
+    if result == 200:
+        cart = complete_cart(user_id)
+        new_cart = add_cart(user_id)
+        if new_cart == None:
+            return jsonify({'error': 'There is already a cart which is not finished.'}), 400 
+        return jsonify({'result': 'Success','cart_id':new_cart.trans_id}), 200
+    else:
+        return response.content
+
+
+# Validate the admin signin
+@auth.verify_password
+def verify_password(username, password):
+    # TODO: Change check if is admin in the database or not.
+    return username == 'admin' and password == 'asdqwe123'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000) 
